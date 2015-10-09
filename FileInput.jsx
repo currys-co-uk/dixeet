5f67d5395da534a090cdd4f8fdf65681da9d29a3
Michal Sevcik <sevcik.mi@gmail.com>
2015-10-09 13:17:05 +0200

    odebran bordel

--- FileInput.jsx
+++ FileInput.jsx
@@ -1,8 +1,4 @@
 FileInput = React.createClass({
-    componentWillReceiveProps(nextProps) {
-        this.forceUpdate();
-    },
-
     render() {
         return (
             <input type="file" multiple onChange={this.props.onChange} />
