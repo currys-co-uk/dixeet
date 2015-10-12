7c565644b34f473112db7cc3f78283c1a5eda27b
Pavel Rozehnal <pavel.rozehnal@dixonscarphone.com>
2015-10-12 09:57:49 +0200

    removed old image store

--- dixeet.jsx
+++ dixeet.jsx
@@ -9,14 +9,7 @@ if (Meteor.isServer) {
   gm(readStream, fileObj.name()).resize('10', '10').stream().pipe(writeStream);
 };*/
 
-var imageStore = new FS.Store.FileSystem("images", {
-  path: "uploads", //optional, default is "/cfs/files" path within app container
-  //transformWrite: myTransformWriteFunction, //optional
-  //transformRead: myTransformReadFunction, //optional
-  maxTries: 5 //optional, default 5
-});
-
-var imageStore1 = new FS.Store.GridFS("images1", {
+var imageStore = new FS.Store.GridFS("images1", {
   //mongoUrl: 'mongodb://127.0.0.1:27017/test/', // optional, defaults to Meteor's local MongoDB
   //mongoOptions: {...},  // optional, see note below
   //transformWrite: myTransformWriteFunction, //optional
@@ -27,7 +20,7 @@ var imageStore1 = new FS.Store.GridFS("images1", {
 });
 
 Images = new FS.Collection("images", {
-  stores: [imageStore1]
+  stores: [imageStore]
 });
 
 
