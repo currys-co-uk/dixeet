e8e9611d0ea1fbbc953e73d8506f6a058192c5c2
Michal Sevcik <sevcik.mi@gmail.com>
2015-10-19 13:10:22 +0200

    Added support for loading more than 200 messages (infinite scroll)

--- client/jsx/App.jsx
+++ client/jsx/App.jsx
@@ -3,24 +3,34 @@ App = React.createClass({
     // This mixin makes the getMeteorData method work
     mixins: [ReactMeteorData],
     messageLimit: 160,
+    limit: 200,
+    threshold: 250,
 
     componentDidMount() {
         setInterval(function () {
             this.setState({appTime: moment()});
         }.bind(this), 30000);
 
-
         Meteor.call('getIP', function (error, result) {
             if (error) {
                 console.log(error);
-            }
-            else {
+            } else {
                 this.setState({ip: result});
             }
         }.bind(this));
+
+        this.attachScrollListener();
     },
 
-    getInitialState: function () {
+    componentDidUpdate() {
+        this.attachScrollListener();
+    },
+
+    componentWillUnmount() {
+        this.detachScrollListener();
+    },
+
+    getInitialState() {
         Meteor.call('getRole', window.location.href, function (error, result) {
             if (error) {
                 console.log(error);
@@ -36,14 +46,11 @@ App = React.createClass({
             role: 'user',
             logins: [],
             messageFilter: null,
+            page: 1,
             hashtags: [],
             appTime: moment(),
             formHidden: true
         };
-    },
-
-    hasRole(role) {
-        return this.state.role == role;
     },
 
     // Loads items from the Tasks collection and puts them on this.data.tasks
@@ -62,7 +69,6 @@ App = React.createClass({
                 return new RegExp(el, 'i')
             });
             query["name"] = {$in: regexs_logins};
-
         }
 
         if (this.state.messageFilter !== null) {
@@ -73,9 +79,40 @@ App = React.createClass({
             query["stream"] = new RegExp(this.state.stream, 'gi');
         }
 
+        var limit = this.limit * this.state.page;
+
         return {
-            tasks: Tasks.find(query, {sort: {createdAt: -1, limit: 200}}).fetch()
+            tasks: Tasks.find(query, {sort: {createdAt: -1}, limit: limit}).fetch()
         }
+    },
+
+    scrollListener() {
+        var el = React.findDOMNode(this.refs.tasksList);
+
+        var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        var willLoad = el.offsetTop + el.offsetHeight - scrollTop - window.innerHeight < Number(this.threshold);
+
+        if (willLoad) {
+            this.detachScrollListener();
+            this.setState({page: this.state.page + 1});
+        }
+    },
+
+    attachScrollListener: function () {
+        setTimeout(function() {
+            window.addEventListener('scroll', this.scrollListener);
+            window.addEventListener('resize', this.scrollListener);
+            this.scrollListener();
+        }.bind(this), 200);
+    },
+
+    detachScrollListener: function () {
+        window.removeEventListener('scroll', this.scrollListener);
+        window.removeEventListener('resize', this.scrollListener);
+    },
+
+    hasRole(role) {
+        return this.state.role == role;
     },
 
     logoClick() {
@@ -151,7 +188,7 @@ App = React.createClass({
                          onStreamClick={this.setStream}/>;
         });
 
-        return <ul>{tasks}</ul>;
+        return <ul ref="tasksList">{tasks}</ul>;
     },
 
     handleSearchSubmit(text) {
@@ -191,7 +228,7 @@ App = React.createClass({
                     logoClick={this.logoClick}
                     />
 
-                {this.renderTasks()}
+                    {this.renderTasks()}
             </div>
         );
     }
